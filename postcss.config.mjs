547b9b6e7f7613bed931c4fa6ea8a591b9453178
osama <om2951855@gmail.com>
2025-03-31 01:40:32 +0200

    update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,6 @@
 const config = {
     plugins: ["@tailwindcss/postcss"],
     theme: {
-        extend: {
-            backgroundImage: {
-                "custom-bg": "url('/back.png')",
-            },
-        },
     },
 };
 
