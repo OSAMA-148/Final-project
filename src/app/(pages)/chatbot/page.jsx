697b2c1ee297c4696cbb26aee9f56548e20593de
osama <om2951855@gmail.com>
2025-04-04 18:25:49 +0200

    fix chatbot

--- src/app/(pages)/chatbot/page.jsx
+++ src/app/(pages)/chatbot/page.jsx
@@ -64,7 +64,7 @@ const handleSubmit = async (e) => {
     const handleNewChat = () => setMessages([]);
 
     return (
-        <div className="w-[80%] rounded-lg p-4 mb-56 bg-transparent">
+        <div className="w-[80%] rounded-lg p-4 mb-[238px] bg-transparent">
             <div className="flex justify-end items-center mb-1">
                 <motion.button
                     whileTap={{ scale: 0.9 }}
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
                 </motion.button>
             </div>
 
-            <div className="h-50 overflow-y-auto p-4 rounded-lg flex flex-col border border-gray-400 shadow-lg">
+            <div className="h-[185px] overflow-y-auto p-4 rounded-lg flex flex-col border border-gray-400 shadow-lg">
                 {messages.map((msg, index) => (
                     <motion.div
                         key={index}
