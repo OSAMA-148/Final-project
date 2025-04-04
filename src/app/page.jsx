4aee2c09f44f6e60faadbe1b2a6d2bd2ec5d71d4
osama <om2951855@gmail.com>
2025-04-04 19:36:11 +0200

    fix: update login button background color for better visibility

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
             >
                 <Link
                     href="/login"
-                    className="bg-blue-700 w-48 py-2 px-32 rounded-full text-2xl text-white text-center min-w-[200px] hover:bg-blue-800 transition duration-300"
+                    className="bg-blue-500 w-48 py-2 px-32 rounded-full text-2xl text-white text-center min-w-[200px] hover:bg-blue-600 transition duration-300"
                 >
                     Login
                 </Link>
