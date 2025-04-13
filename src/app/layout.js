8b740a9bd9992c13b7a5bbe64cc0ca9a86450ed7
osama <om2951855@gmail.com>
2025-04-13 11:22:38 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className="antialiased">
-                <div className="bg-[url(https://i.imgur.com/TfuFSCF.jpeg)] bg-cover bg-center h-screen w-screen overflow-hidden bg-fixed">
+                <div className="background bg-[url(https://i.imgur.com/TfuFSCF.jpeg)] bg-cover bg-center h-screen w-screen overflow-hidden bg-fixed">
                     <ToastContainer position="top-center" autoClose={2000} />
                     <ContextProviders>{children}</ContextProviders>
                 </div>
